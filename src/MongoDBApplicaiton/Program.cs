1270bf7ab106cb391fe0a48e2798a907df6c691c
Administrator <Administrator@WIN-J5U3BOBSQ8O>
2017-07-19 15:26:07 +0100

    Add project files.

---
+++ src/MongoDBApplicaiton/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using FluentAssertions;
+
+namespace MongoDBApplicaiton
+{
+
+    public class TestMongoDB
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public  static void TestData()
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("test");
+
+            var document = new BsonDocument
+            {
+                {
+                    "address", new BsonDocument
+                    {
+                        {"street", "2 Avenue"},
+                        {"zipcode", "10075"},
+                        {"building", "1480"},
+                        {"coord", new BsonArray {73.9557413, 40.7720266}}
+                    }
+                },
+                {"borough", "Manhattan"},
+                {"cuisine", "Italian"},
+                {
+                    "grades", new BsonArray
+                    {
+                        new BsonDocument
+                        {
+                            {"date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc)},
+                            {"grade", "A"},
+                            {"score", 11}
+                        },
+                        new BsonDocument
+                        {
+                            {"date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc)},
+                            {"grade", "B"},
+                            {"score", 17}
+                        }
+                    }
+                },
+                {"name", "Vella"},
+                {"restaurant_id", "41704620"}
+            };
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            collection.InsertOneAsync(document);
+
+            var filter = new BsonDocument();
+            var count = 0;
+            string value = "Empty";
+            using (var cursor = collection.FindAsync(filter).Result)
+            {
+                while (cursor.MoveNextAsync().Result)
+                {
+                    var batch = cursor.Current;
+                    foreach (var fetcheddocument in batch)
+                    {
+                        // process document
+                        count.Should().Be(25359);
+
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    public class TestAttributeData
+    {
+       [Required]
+        public DateTime? date { get; set; }
+    }
+    public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+
+            TestAttributeData testAttribute=new TestAttributeData();
+           // testAttribute.date=DateTime.UtcNow;
+            var vc=new ValidationContext(testAttribute,null,null);
+            List<System.ComponentModel.DataAnnotations.ValidationResult> result=new List<ValidationResult>();
+            var isvalid = Validator.TryValidateObject(testAttribute, vc, result,true);
+
+            TestMongoDB.TestData();
+        }
+    }
+}
---
+++ src/MongoDBApplicaiton/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MongoDBApplicaiton")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("009d9606-2ac3-48cb-99e7-d69250a57b65")]
