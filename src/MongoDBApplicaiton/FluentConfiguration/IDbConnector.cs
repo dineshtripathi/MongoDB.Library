f29ecf091a1765af2c342a327300a807aeeb0c94
dinesh tripathi <dinesh.tripathi@aaditricorp.com>
2017-07-20 16:45:37 +0100

    added interface contract

--- src/MongoDBApplicaiton/FluentConfiguration/IDbConnector.cs
+++ src/MongoDBApplicaiton/FluentConfiguration/IDbConnector.cs
@@ -1,7 +1,12 @@
-﻿namespace MongoDBApplicaiton.FluentConfiguration
+﻿using MongoDB.Driver.Core.Authentication;
+
+namespace MongoDBApplicaiton.FluentConfiguration
 {
     public interface IDbConnector
     {
         IDbConnector Configure();
+        IDbConnector WithCredentials(UsernamePasswordCredential credential);
+        IDbConnector ForDatabase(string databaseName);
+        bool IsConnectionSucceeded();
     }
 }
